7a3826cb7cb00b50a06350398a7c9be1e0519897
Viktor Recour <viktor@mylighthouse.com>
2024-02-29 21:38:38 +0100

    finish get exercise

--- EXERCISE-LL-Get.js
+++ EXERCISE-LL-Get.js
@@ -103,12 +103,16 @@ class LinkedList {
         return temp;
     }
  
-	/// WRITE GET METHOD HERE ///
-	//                         //
-	//                         //
-	//                         //
-	//                         //
-	/////////////////////////////
+	get(index) {
+        if (index < 0 || index > this.length) return undefined;
+        let temp = this.head;
+
+        for (let i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
 
  }
  
