6650cda13cdccb06c1646697988e6c52b13c1ce3
Viktor Recour <viktor@mylighthouse.com>
2024-02-29 21:47:30 +0100

    finish set exercise

--- EXERCISE-LL-Set.js
+++ EXERCISE-LL-Set.js
@@ -112,12 +112,16 @@ class LinkedList {
         return temp;
     }
  
-	/// WRITE SET METHOD HERE ///
-	//                         //
-	//                         //
-	//                         //
-	//                         //
-	/////////////////////////////
+	set(index, value) {
+        let temp = this.get(index);
+
+        if (temp) {
+            temp.value = value;
+            return true;
+        }
+
+        return false;
+    }
  
 }
  
