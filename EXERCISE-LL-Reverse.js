99f833c2763f8c5f23d01c40b4af1b3afbd1b398
Viktor Recour <viktor@mylighthouse.com>
2024-02-29 22:36:17 +0100

    finish reverse exercise

--- EXERCISE-LL-Reverse.js
+++ EXERCISE-LL-Reverse.js
@@ -148,12 +148,23 @@ class LinkedList {
         return temp;
     }
 
-	/// WRITE REVERSE METHOD HERE ///
-	//                             //
-	//                             //
-	//                             //
-	//                             //
-	///////////////////////////////// 
+	reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+
+        let next = temp.next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+
+        return this;
+    }
 
 }
 
