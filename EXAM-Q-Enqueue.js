8c64cc6a5993c188dcb6835c4910282a0c58b427
Viktor Recour <viktor@mylighthouse.com>
2024-03-11 22:01:15 +0100

    finish enqueue exam

--- EXAM-Q-Enqueue.js
+++ EXAM-Q-Enqueue.js
@@ -42,12 +42,17 @@ class MyQueue {
       return this.stack1.isEmpty();
   }
 
-  // WRITE THE ENQUEUE METHOD HERE //
-  //                               //
-  //                               //
-  //                               //
-  //                               //
-  ///////////////////////////////////
+  enqueue(value) {
+    while (!this.stack1.isEmpty()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    this.stack2.push(value);
+
+    while (!this.stack2.isEmpty()) {
+      this.stack1.push(this.stack2.pop());
+    }
+  }
 
 }
 
