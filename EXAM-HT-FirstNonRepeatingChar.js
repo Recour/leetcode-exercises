0bf2362c2b07b9bdd20cfdc2e64ff5c7ecdc3991
Viktor Recour <viktor@mylighthouse.com>
2024-03-17 18:48:37 +0100

    add firstNonRepeatingChar exam

---
+++ EXAM-HT-FirstNonRepeatingChar.js
@@ -0,0 +1,18 @@
+//   +=====================================================+
+//   |                WRITE YOUR CODE HERE                 |
+//   | Description:                                        |
+//   | - This function finds the first non-repeating       |
+//   |   character in a string.                            |
+//   |                                                     |
+//   | Return type: string/null                            |
+//   | - Returns the first non-repeating character if      |
+//   |   found, otherwise returns null.                    |
+//   |                                                     |
+//   | Tips:                                               |
+//   | - You can use either a Map or an object to count    |
+//   |   the occurrences of each character.                |
+//   | - Example with Map:                                 |
+//   |   charCounts.set(c, (charCounts.get(c) || 0) + 1);  |
+//   | - Example with object:                              |
+//   |   charCounts[c] = (charCounts[c] || 0) + 1;         |
+//   +=====================================================+
