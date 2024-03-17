b1c17c7a15256634bcb8c7ee983acfcd28844127
Viktor Recour <viktor@mylighthouse.com>
2024-03-17 18:48:54 +0100

    add firstNonRepeatingChar exam correctly

--- EXAM-HT-FirstNonRepeatingChar.js
+++ EXAM-HT-FirstNonRepeatingChar.js
@@ -16,3 +16,57 @@
 //   | - Example with object:                              |
 //   |   charCounts[c] = (charCounts[c] || 0) + 1;         |
 //   +=====================================================+
+
+
+
+
+
+// ---------------
+// All Unique
+// ---------------
+console.log("All Unique:");
+console.log("Input: 'abcde'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('abcde')));
+console.log("---------------");
+
+// ---------------
+// Some Duplicates
+// ---------------
+console.log("Some Duplicates:");
+console.log("Input: 'aabbccdef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('aabbccdef')));
+console.log("---------------");
+
+// ---------------
+// All Duplicates
+// ---------------
+console.log("All Duplicates:");
+console.log("Input: 'aabbcc'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('aabbcc')));
+console.log("---------------");
+
+// ---------------
+// Empty String
+// ---------------
+console.log("Empty String:");
+console.log("Input: ''");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('')));
+console.log("---------------");
+
+// ---------------
+// Single Character
+// ---------------
+console.log("Single Character:");
+console.log("Input: 'a'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('a')));
+console.log("---------------");
+
+// ---------------
+// Multiple Non-Repeating Chars
+// ---------------
+console.log("Multiple Non-Repeating Chars:");
+console.log("Input: 'abcdaef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar('abcdaef')));
+console.log("---------------");
+
+
